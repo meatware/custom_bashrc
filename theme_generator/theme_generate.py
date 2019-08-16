1abe225cd83b80e32c8b63e8d3094a44f8cec3c8
Girish <girishtampi@firmstep.com>
2019-08-16 19:39:45 +0100

    got theme generator working mostly - need to make colsw less painful

--- theme_generator/theme_generate.py
+++ theme_generator/theme_generate.py
@@ -4,8 +4,8 @@ from itertools import permutations
 col_dict = {}
 col_names = []
 
-#col_defs = "./colour_defs.txt"
-col_defs = "./colour_defs_large.txt"
+col_defs = "./colour_defs.txt"
+#col_defs = "./colour_defs_large.txt"
 with open(col_defs, 'r') as FHO:
     for line in FHO:
         split_line = line.split()
@@ -23,6 +23,12 @@ with open(col_defs, 'r') as FHO:
             col_names.append(col_key)
 
 print()
+
+# remove colours taht don't work on black screen
+# (large list) del_list = ["Black", "BBlack", "IBlack", "BIBlack", "White", "BWhite", "IWhite", "BIWhite"] #, "", "", "", "", "", "", ""]
+del_list = ["GRAY", "DKGRAY", "HIBlack", "HIWhite"] 
+for rm_col in del_list:
+    col_names.remove(rm_col)
 
 #for key, value in col_dict.items():
     #print(key, value)
@@ -42,18 +48,24 @@ macro_bar = "BARCOL_ARR=("
 macro_text = "TXTCOL_ARR=("
 for combo in perm_list:
     print(combo)
-    macro_bar = macro_bar + "${" + combo[0] + "} "
-    macro_text = macro_text + "${" + combo[1] + "} "
+
+    macro_bar = macro_bar + """'${""" + combo[0] + """}' """
+    macro_text = macro_text + """'${""" + combo[1] + """}' """
     #print("jj", macro_bar)
 macro_bar = macro_bar + ")"
 macro_text = macro_text + ")"
 
-print("\n", macro_bar)
-print("\n", macro_text)
+print()
+print(macro_bar) # goes into custom_bashrc/bashrc_assets/_bash_colour_defs.sh
+print()
+print(macro_text) # goes into custom_bashrc/bashrc_assets/_bash_colour_defs.sh
 
+print()
+print("len(macro_bar)", len(macro_bar))
+print("len(macro_text)", len(macro_text))
 
-
-print("\nlen of array for autokey script: autokey_colour_cycle_script.py ", len(perm_list))
+# members in array
+print("no of colour permutations ", len(perm_list))
 
 #print(perm_list)
 
