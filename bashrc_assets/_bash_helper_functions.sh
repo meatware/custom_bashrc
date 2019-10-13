0eb7153b3843c6fba0801baf16e1f941e29246b6
Girish <girishtampi@firmstep.com>
2019-10-13 11:34:28 +0100

    add git fetch to gety up to date remote info

--- bashrc_assets/_bash_helper_functions.sh
+++ bashrc_assets/_bash_helper_functions.sh
@@ -1,4 +1,5 @@
 function check_new_bashrc_vers() {
+    git --git-dir=${HOME}/custom_bashrc/.git fetch --quiet 
     # check relation of our local .bashrc to remote basshrc at https://github.com/meatware/custom_bashrc
     BASHRC_CURR_BRANCH=$(git --git-dir=${HOME}/custom_bashrc/.git rev-parse --abbrev-ref HEAD)
     BASHRC_COMMIT_DETAILS=$(git --git-dir=${HOME}/custom_bashrc/.git rev-list --left-right \
